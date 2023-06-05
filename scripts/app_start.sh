3c84bba747d2566ef8ad141d255d3143247fadf1
CTO at RNS <129362492+rnscto@users.noreply.github.com>
2023-06-05 16:16:01 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -6,5 +6,5 @@ cd /home/ec2-user/lms
 #_Delete_Old_PM2_Service
 #sudo pm2 delete Frontend
 #sudo pm2 start server.js --name Frontend
-pm2 delete Frontend
-pm2 start server.js --name Frontend
+sudo pm2 delete Frontend
+sudo pm2 start server.js --name Frontend

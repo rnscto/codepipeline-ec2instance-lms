a46c29d8b659b2b0074b304f475e8f26693b587f
CTO at RNS <129362492+rnscto@users.noreply.github.com>
2023-06-05 11:31:42 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #_Change_Working_Directory
-cd /home/ec2-user/server
+cd /home/ec2-user/lms
 
 #_Remove_Unused_Code
 rm -rf node_modules
@@ -10,4 +10,4 @@ rm -rf build
 #Install_node_modules_&_Make_React_Build
 npm  install
 npm run build
-cp -rf /home/ec2-user/server/build/* /web/lms
+cp -rf /home/ec2-user/lms/build/* /web/lms
